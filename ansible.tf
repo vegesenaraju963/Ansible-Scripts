2f1d508ab5373b42aee2036d2e9c3aa94e4a9d38
vegesenaraju963 <82093130+vegesenaraju963@users.noreply.github.com>
2021-04-26 19:29:09 +0530

    Update ansible.tf

--- ansible.tf
+++ ansible.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "master" {
   ami           = "ami-0affd4508a5d2481b"
   instance_type = "t2.micro"
   key_name = "lab"
-  vpc_security_group_ids = ["sg-0ba63e7078eb46de6"]
+  vpc_security_group_ids = ["sg-05393d12adc992534"]
   user_data = "script.sh"
   tags = {
     Name = "Ansible master"
